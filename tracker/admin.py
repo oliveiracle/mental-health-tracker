635f1a94b1f9c5a09bdc748bf1a36d1096aa821a
Cleino Frank <cleinofrank@gmail.com>
2026-01-29 22:12:08 +0000

    feat: Register MoodEntry model in Django admin with custom display

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import MoodEntry
 
-# Register your models here.
+
+@admin.register(MoodEntry)
+class MoodEntryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'mood_score', 'created_at')
+    list_filter = ('date', 'mood_score')
+    search_fields = ('user__username', 'notes')
+    readonly_fields = ('created_at', 'updated_at')
+    ordering = ('-date',)
