16b2848f091bafd89c6b4ca22b984e707f49a08e
Cleino Frank <cleinofrank@gmail.com>
2026-02-02 14:41:55 +0000

    feat: add URL for mood list page

--- tracker/urls.py
+++ tracker/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),  # home page
     path('register/', views.register, name='register'),  # registration page
     path('logout/', views.logout_view, name='logout'),  # logout url
+    path('moods/', views.mood_list, name='mood_list'),  # mood entries list
 ]
