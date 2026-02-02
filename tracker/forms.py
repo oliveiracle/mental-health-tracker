0a9bf2068527a3200d7deee89a687748a40710d3
Cleino Frank <cleinofrank@gmail.com>
2026-02-02 14:41:34 +0000

    feat: add MoodEntryForm to forms

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import MoodEntry
 
 
 class RegisterForm(UserCreationForm):
@@ -9,3 +10,10 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+
+# form for mood entry
+class MoodEntryForm(forms.ModelForm):
+    class Meta:
+        model = MoodEntry
+        fields = ['mood_score', 'notes']
