767ad7e307d511e821e00b20908755075c64fe6e
Cleino Frank <cleinofrank@gmail.com>
2026-02-02 19:15:19 +0000

    Add url for mood list

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,11 +1,9 @@
-# URL patterns for tracker app
 from django.urls import path
 from . import views
 
-# define url patterns
 urlpatterns = [
-    path('', views.home, name='home'),  # home page
-    path('register/', views.register, name='register'),  # registration page
-    path('logout/', views.logout_view, name='logout'),  # logout url
-    path('moods/', views.mood_list, name='mood_list'),  # mood entries list
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+    path('mood/', views.mood_list, name='mood_list'),
 ]
