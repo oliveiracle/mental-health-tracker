9277a50e604df113fc84f7cffd1881cded311f06
Cleino Frank <cleinofrank@gmail.com>
2026-02-03 20:25:27 +0000

    Add URL for mood create page

--- tracker/urls.py
+++ tracker/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('mood/', views.mood_list, name='mood_list'),
+    path('mood/add/', views.mood_create, name='mood_create'),
 ]
