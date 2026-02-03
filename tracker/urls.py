2e20100d6ff8f4ae42868d2893d873d8c0f81263
Cleino Frank <cleinofrank@gmail.com>
2026-02-03 20:57:40 +0000

    Add edit and delete mood routes

--- tracker/urls.py
+++ tracker/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('mood/', views.mood_list, name='mood_list'),
     path('mood/add/', views.mood_create, name='mood_create'),
+    path('mood/<int:pk>/edit/', views.mood_edit, name='mood_edit'),
+    path('mood/<int:pk>/delete/', views.mood_delete, name='mood_delete'),
 ]
